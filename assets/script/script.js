a32505bed9d9757fc5df4b51bfc4364b2434e4a2
ktaeyvne <98595992+ktaeyvne@users.noreply.github.com>
2022-12-13 13:41:05 +0700

    Delete assets/script directory

--- assets/script/script.js
+++
@@ -1,8 +0,0 @@
-const bar = document.querySelector('nav.smartphone i.fa-bars');
-
-const nav = document.querySelector('nav.desktop');
-
-bar.addEventListener('click', () => {
-    nav.classList.toggle('active');
-    bar.classList.toggle('active2');
-});
